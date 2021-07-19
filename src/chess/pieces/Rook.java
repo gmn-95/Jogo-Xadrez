18949d1f0f9dc2d98eddf21e6e564c6914caf6b5
Gustavo Macedo <gmnascimento.95@outlook.com.br>
2021-07-19 19:53:48 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -18,4 +18,10 @@ public class Rook extends ChessPiece{ // torre
 		return rook;
 	}
 	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+	
 }
