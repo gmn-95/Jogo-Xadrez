ed052a71d6b4ac53ff84dd39fec20f592a9afd1b
Gustavo Macedo <gmnascimento.95@outlook.com.br>
2021-07-21 21:34:53 -0300

    Ignore Eclipe folder and Classpath JavaSE-11

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,7 @@ public class Program {
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = sc.nextLine();
+					type = type.toUpperCase();
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
